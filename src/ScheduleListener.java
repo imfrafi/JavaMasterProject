b5f94182a93db08eb98ce571c131647d838ec6ad
imfrafi <x0180368905@gmail.com>
2022-03-30 16:32:54 +0800

    Update for the format
    by Jenny

--- src/ScheduleListener.java
+++ src/ScheduleListener.java
@@ -32,35 +32,39 @@ public class ScheduleListener implements ActionListener {
             public void actionPerformed( ActionEvent e ){
         
                 String rawtxt = rawData.getText();
-                rawtxt = rawtxt.replaceAll("\r|\n", " ");
-                String[] txt = rawtxt.split( "	" );
-                // for( int i=0; i<txt.length; ++i ){
-                //     if( i%7!=6 ){
-                //         System.out.print( txt[i] + " " );
-                //     }else{
-                //         System.out.println( txt[i] );
-                //     }
-                // }
+                rawtxt = rawtxt.replaceAll("\r|\n|\t", " ");
+                rawtxt = rawtxt.replace("    "," ");
+                System.out.println( rawtxt );
+                String[] txt = rawtxt.split( " " );
+
+                for( int i=0; i<txt.length; ++i ){
+                    // System.out.println( txt[i] );
+                    if( i%7!=6 ){
+                        System.out.print( txt[i] + " " );
+                    }else{
+                        System.out.println( txt[i] );
+                    }
+                }
                 int order=0, number=0, credit=0;
                 String name="", teacher="", place="";
                 ArrayList<Course> courses = new ArrayList<>();
-                for( int i=0; i<txt.length; ++i ){
-                    if( i%7==0 ){
-                        order = i+1;
-                    }else if( i%7==1 ){
-                        number = Integer.parseInt(txt[i]);
-                    }else if( i%7==2 ){
-                        name = txt[i];
-                    }else if( i%7==3 ){
-                        teacher = txt[i];
-                    }else if( i%7==4 ){
-                        credit = Integer.parseInt(txt[i]);
-                    }else if( i%7==5 ){
-                        place = txt[i];
-                    }else{
-                        courses.add( new Course( order, number, name, teacher, credit, place ) );
-                    }
-                }
+                // for( int i=0; i<txt.length; ++i ){
+                //     if( i%7==0 ){
+                //         order = i+1;
+                //     }else if( i%7==1 ){
+                //         number = Integer.parseInt(txt[i]);
+                //     }else if( i%7==2 ){
+                //         name = txt[i];
+                //     }else if( i%7==3 ){
+                //         teacher = txt[i];
+                //     }else if( i%7==4 ){
+                //         credit = Integer.parseInt(txt[i]);
+                //     }else if( i%7==5 ){
+                //         place = txt[i];
+                //     }else{
+                //         courses.add( new Course( order, number, name, teacher, credit, place ) );
+                //     }
+                // }
         
             }
         
